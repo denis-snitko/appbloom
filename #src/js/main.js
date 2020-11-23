3d5ba67d5658989bc7c72a5d0f31483d30127984
Denis Snitko <denissnitko@gmail.com>
2020-11-23 14:16:46 +0300

    Add s-examples

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,1 +1,13 @@
+let swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 100,
 
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
