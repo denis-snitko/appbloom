7d29dbd213736ac5bdd464fe10ae156c92ed4303
Denis Snitko <denissnitko@gmail.com>
2020-11-24 12:10:12 +0300

    Update adaptive

--- docs/js/main.js
+++ docs/js/main.js
@@ -11,3 +11,17 @@ let swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// BURGER
+
+const burgerOpen = document.querySelector('.burger-icon');
+const burgerClose = document.querySelector('.burger__close');
+const burger = document.querySelector('.burger');
+
+burgerOpen.addEventListener('click', () => {
+  burger.setAttribute('style', 'top: 0')
+})
+
+burgerClose.addEventListener('click', () => {
+  burger.setAttribute('style', 'top: -100%')
+})
