7150cb0b3ee2a8de268cbe868bb88f8f71d31a67
Denis Snitko <denissnitko@gmail.com>
2020-11-24 12:27:50 +0300

    Add smooth scroll

--- #src/js/main.js
+++ #src/js/main.js
@@ -25,3 +25,20 @@ burgerOpen.addEventListener('click', () => {
 burgerClose.addEventListener('click', () => {
   burger.setAttribute('style', 'top: -100%')
 })
+
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
